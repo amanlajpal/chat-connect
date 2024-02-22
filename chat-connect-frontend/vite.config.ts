ab5e79180ac6227805b33f8540ebb25bcdfd246f
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2024-02-22 22:13:52 +0530

    initialize-project: Add PostCSS configuration and utility function

--- chat-connect-frontend/vite.config.ts
+++ chat-connect-frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+// This file is used to configure Vite, a build tool for modern web development.
+import path from "path"
 import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
