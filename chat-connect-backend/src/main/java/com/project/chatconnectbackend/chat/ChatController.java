8bfd2c30eed630906b886bdc415a0d340d4e3e43
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2024-03-14 20:22:39 +0530

    feat: Add Cross-Origin support and logging in ChatController and set preserve publish order in WebSocketConfig

--- chat-connect-backend/src/main/java/com/project/chatconnectbackend/chat/ChatController.java
+++ chat-connect-backend/src/main/java/com/project/chatconnectbackend/chat/ChatController.java
@@ -5,14 +5,23 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class ChatController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         // TODO -- to be implemented
+        logger.info("Message sent: " + chatMessage.getContent());
+        logger.info("Message sent by: " + chatMessage.getSender());
+        logger.info("Message type: " + chatMessage.getType());
         return chatMessage;
     }
 
@@ -20,9 +29,11 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(
             @Payload ChatMessage chatMessage,
-            SimpMessageHeaderAccessor headerAccessor
-    ) {
+            SimpMessageHeaderAccessor headerAccessor) {
+
         // TODO -- to be implemented
+        logger.info("User added in Chatroom: " + chatMessage.getSender());
+        logger.info("User added in Chatroom: " + headerAccessor);
         return chatMessage;
     }
 }
--- chat-connect-backend/src/main/java/com/project/chatconnectbackend/config/WebSocketConfig.java
+++ chat-connect-backend/src/main/java/com/project/chatconnectbackend/config/WebSocketConfig.java
@@ -12,12 +12,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry
+                .addEndpoint("/ws") // Endpoint for client to connect to server
+                .setAllowedOriginPatterns("*") // Allow all origins to connect
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app"); // prefix for client to send message to server
         registry.enableSimpleBroker("/topic"); // prefix for server to send message to client
+        registry.setPreservePublishOrder(true); // Set to true to preserve the order of messages being sent to clients
     }
 }
